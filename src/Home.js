d58dce9c73e121bb262db45d35780c34fb80e009
yarzar.bhone <yarzar.bhone@frontiir.net>
2024-08-28 13:58:54 +0630

    u

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,33 @@
+// import { useState } from "react";
+// import axios from "axios";
+
 export default function Home() { 
+    // const [receive, setReseive] = useState();
+    // const [text, setText] = useState();
+
+    // const Api = () => {
+    //     axios.get('http://localhost:5000/data')
+    //     .then(response => {
+    //     const redata = response.data;
+    //     setReseive(redata);
+    //     console.log('Response!!:', redata);
+    //     })
+    //     .catch(error => {
+    //     console.error(error);
+    //     alert(error);
+    //     });
+    // }
+
     return(
         <div style={{marginLeft:'10px'}}>
             <h1> Welcome </h1> <br/> <br/>
+            {/* <button onClick={Api}> Test </button> <br/>
+            <h2> {receive} </h2> */}
         </div>
     );
 }
+
+
 
 
 
