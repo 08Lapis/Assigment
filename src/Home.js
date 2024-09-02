0307be75f81e25bc1991ec05e3dba01a60d07783
yarzar.bhone <yarzar.bhone@frontiir.net>
2024-09-02 15:54:19 +0630

    m

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,17 @@
 // import { useState } from "react";
-// import axios from "axios";
+// import Calculate from "./Calculate";
+
 
 export default function Home() { 
-    // const [receive, setReseive] = useState();
-    // const [text, setText] = useState();
-
-    // const Api = () => {
-    //     axios.get('http://localhost:5000/data')
-    //     .then(response => {
-    //     const redata = response.data;
-    //     setReseive(redata);
-    //     console.log('Response!!:', redata);
-    //     })
-    //     .catch(error => {
-    //     console.error(error);
-    //     alert(error);
-    //     });
-    // }
+    // const [num1] = useState(1);
+    // const [num2] = useState(1);
+    // const [count, setCount] = useState(0);
 
     return(
         <div style={{marginLeft:'10px'}}>
             <h1> Welcome </h1> <br/> <br/>
-            {/* <button onClick={Api}> Test </button> <br/>
-            <h2> {receive} </h2> */}
+            {/* <button onClick={() => setCount(count+1)}> count : {count} </button> <br/>
+            <Calculate num1={num1} num2={num2} /> */}
         </div>
     );
 }
